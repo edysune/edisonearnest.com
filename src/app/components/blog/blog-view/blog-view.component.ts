1079cd68ae64652db77e43e881d68ab99031e9dc
Edison Earnest <edisongearnest@gmail.com>
2022-02-23 22:45:56 -0700

    fixing some styling issues

--- src/app/components/blog/blog-view/blog-view.component.ts
+++ src/app/components/blog/blog-view/blog-view.component.ts
@@ -13,7 +13,7 @@ export class BlogViewComponent implements OnInit {
   public blogs: blog[] = [
     {
       image: "../../assets/edisonearnest_assets/nav-assets/blog_assets/stay_safe.jpg",
-      title: "Life with Covid - Early 2022",
+      title: "Life with Covid",
       description: "Still Alive - Just an update with Covid from 2019-2022.",
       date: new Date(2022, 0, 9),
       tags: ["covid", "life"]
