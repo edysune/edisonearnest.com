3b7ac82943d93ae8b1524dd0c50d99efcc4003d0
Edison <edisongearnest@gmail.com>
2024-02-23 00:46:10 -0700

    removing comments, fixing mobile sizing for ingredients

--- src/components/recipes/RenderIngredientSection.js
+++ src/components/recipes/RenderIngredientSection.js
@@ -1,9 +1,11 @@
+import tw from "twin.macro";
 import { SectionHeadingLeftWithBottomSpacing } from "components/misc/Headings.js";
 import React from "react";
 import './RenderRecipe.css';
 
+const Item = tw.div`lg:w-1/2 sm:w-5/6`;
+
 function checkboxEventHandler(event) {
-  console.log("HELLO WORLD")
   event.currentTarget.classList.toggle('checked-ingrediant');
   event.currentTarget.classList.toggle('unchecked-ingrediant');
 }
@@ -16,13 +18,13 @@ const RenderIngredientSection = ({ //eslint-disable-line
   <div>
     <SectionHeadingLeftWithBottomSpacing>Ingredients</SectionHeadingLeftWithBottomSpacing>
     {recipeMatch?.ingredients.map((ingredient) => (
-      <div
+      <Item
         onClick={checkboxEventHandler}
         className="unchecked-ingrediant ingrediant-item"
         sx={{ margin: '10px', fontWeight: 'bold' }}
       >
         {ingredient}
-      </div>
+      </Item>
     ))}
   </div>
   );
