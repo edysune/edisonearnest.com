a2a5ccd7d11f2c7f6b56ec99118ac95d06549fdd
Edison <edisongearnest@gmail.com>
2024-02-24 00:50:38 -0700

    fix linting issues

--- src/components/recipes/RecipeOverviewCard.js
+++ src/components/recipes/RecipeOverviewCard.js
@@ -1,39 +1,14 @@
 import * as React from 'react';
-import RestaurantIcon from '@mui/icons-material/Restaurant'; //Prepare
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'; //Cook
-import CompressIcon from '@mui/icons-material/Compress';
-import ScaleIcon from '@mui/icons-material/Scale';
-import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import tw from "twin.macro";
 
 export default function RecipeOverviewCard({ //eslint-disable-line
     info = null
 }) {
     const InfoContainer = tw.div` w-full max-w-md mx-auto md:max-w-none md:mx-0`;
-    const InfoItemBox = tw.div`inline-block align-top w-1/3 h-full`;
+    const InfoItemBox = tw.div`inline-block align-top w-1/3`;
     const InfoItemBoxBorder = tw(InfoItemBox)`border-r border-[#0]`;
     const Item = tw.p`m-5`;
     const ItemLabel = tw.span`font-extrabold`;
-
-    function GetIconFromType(iconType) {
-        const fs = 70;            
-        switch (iconType) {
-            case 'prepTime':
-                return <RestaurantIcon sx={{ fontSize: fs }} />;
-            case 'cookTime':
-                return <AccessAlarmIcon sx={{ fontSize: fs }} />;
-            case 'pressureCookTime':
-                return <CompressIcon sx={{ fontSize: fs }} />;
-            case 'servings':
-                return <ScaleIcon sx={{ fontSize: fs }} />;
-            default:
-                return <QuestionMarkIcon sx={{ fontSize: fs }} />;
-        }
-    }
-
-    function GenItemBox(hasBorder = true) {
-        return hasBorder ? InfoItemBox : InfoItemBox;
-    }
 
     function GenItem(label, value, units = 'N/A', hideOnNull = false) {
         if (hideOnNull && (!value || value === 0 || value === 'N/A')) { return; }
--- src/components/recipes/RenderInfoSection.js
+++ src/components/recipes/RenderInfoSection.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Box, Stack} from '@mui/material';
 import RecipeOverviewCard from 'components/recipes/RecipeOverviewCard';
 import './RenderRecipe.css';
 
