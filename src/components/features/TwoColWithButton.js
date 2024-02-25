5acfa54c584891cc3765dc2dd97158bb4582ff78
Edison <edisongearnest@gmail.com>
2024-02-25 09:17:14 -0700

    removing more unused images from teact

--- src/components/features/TwoColWithButton.js
+++ src/components/features/TwoColWithButton.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
 import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
 import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
-import TeamIllustrationSrc from "images/team-illustration-2.svg";
 import {ReactComponent as SvgDotPattern } from "images/dot-pattern.svg"
 
 const Container = tw.div`relative`;
@@ -49,7 +48,7 @@ export default ({ //eslint-disable-line
   description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   primaryButtonText = "",
   primaryButtonUrl = "",
-  imageSrc = TeamIllustrationSrc,
+  imageSrc = null,
   buttonRounded = true,
   imageRounded = true,
   imageBorder = false,
