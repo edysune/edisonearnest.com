bd24231a678fd22ff39ce96b1b15144aab071a9a
Edison Earnest <edisongearnest@gmail.com>
2024-02-24 22:58:18 -0700

    removing more unused files

--- src/components/misc/Links.js
+++
@@ -1,3 +0,0 @@
-import tw from "twin.macro";
-
-export const PrimaryLink = tw.a`cursor-pointer font-bold text-[#013a6a] border-b-2 border-transparent hocus:border-[#013a6a] hocus:text-primary-800 transition duration-300`;
